f647cb94619efec51318de7cfe1904c62371db9d
Ruben <ruben@iesmordefuentes.com>
2024-06-16 20:23:36 +0200

    Cambios imagenes

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,15 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'imagenes' => [
+            'driver' => 'local',
+            'root' => public_path('imagenes'),
+            'url' => env('APP_URL') . '/imagenes',
             'visibility' => 'public',
             'throw' => false,
         ],
